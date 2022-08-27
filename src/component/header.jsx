0c51d07910b64c1448928136eaaaf0b06e77678f
Farai Major <faraimutukumira@gmail.com>
2022-08-26 23:12:31 -0400

    responsive navbar

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,30 +1,48 @@
 import React from "react";
+import { useState } from "react";
 import "../styles/header.css"
 
 export default function Header() {
+    const [isNavExpanded, setIsNavExpanded] = useState(false)
     return (
-        <div className="navBar" id='home'>
-
-            <ul className="navList">
-                <li className="nav-name">
-                    <a href="#home" id='farai'>Farai</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#about">About</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#skills">Skills</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#projects">Projects</a>
-                </li>
-
-
-                <li className="nav-item">
-                    <a href="#contact">Contact</a>
-                </li>
-            </ul>
-        </div>
+        <nav className="navBar" id='home'>
+            <a href="#home" id='farai'>FARAI</a>
+            <button className="hamburger" onClick={() => {
+                setIsNavExpanded(!isNavExpanded);
+            }}>
+                {/* icon from heroicons.com */}
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="white"
+                >
+                    <path
+                        fillRule="evenodd"
+                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                        clipRule="evenodd"
+                    />
+                </svg>
+            </button>
+            <div className={
+                isNavExpanded ? "navList expanded" : "navList"
+            }>
+                <ul>
+                    <li className="nav-item">
+                        <a href="#about">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#skills">Skills</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#projects">Projects</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
 
     )
 }
