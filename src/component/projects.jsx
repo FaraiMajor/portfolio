7c29d2ff7433d8ed42fb697f3f64eacdc14b7a99
Farai Major <faraimutukumira@gmail.com>
2023-03-25 22:35:09 -0400

    added a project

--- src/component/projects.jsx
+++ src/component/projects.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import weather from '../assets/weatherranger.png';
 import memories from '../assets/memories.png';
 import tenzie from '../assets/tenzies.png'
+import noteapp from '../assets/note-app.png'
 import "../styles/projects.css"
 
 export default function Projects() {
@@ -14,11 +15,47 @@ export default function Projects() {
                 <div className="projectsContainer sectionContainer">
                     <article class="workBox">
                         <div class="imageContainer">
+                            <img src={weather} alt="weather" />
+                        </div>
+                        <div class="describeContainer">
+                            <h3>Weather Rangers</h3>
+                            <h4>Technologies Used: <span class="techStack">React</span>, <span class="techStack">REST API</span>, <span class="techStack">NodeJS</span>, <span class="techStack">PostgresSQL</span></h4>
+                            <ul>
+                                <li>Completely built with PERN stack</li>
+                                <li>Displays daily weather forecast for your current location using geotags </li>
+                                <li>Allows users to create an account and save their favorite locations in their account</li>
+                            </ul>
+                            <div class="projectLinks">
+                                <a href="https://weather-rangers-frontend.herokuapp.com/" class="liveLink" target="_blank" rel="noopener noreferrer">Live</a>
+                                <a href="https://github.com/FaraiMajor/WeatherRangers_frontend" class="repoLink" target="_blank" rel="noopener noreferrer">Repo</a>
+                            </div>
+                        </div>
+                    </article>
+                    <article class="workBox">
+                        <div class="imageContainer">
+                            <img src={noteapp} alt="weather" />
+                        </div>
+                        <div class="describeContainer">
+                            <h3>Note App</h3>
+                            <h4>Technologies Used: <span class="techStack">React</span>,<span class="techStack">CSS</span></h4>
+                            <ul>
+                                <li>React app to make notes</li>
+                                <li>Users can write, read, search, save and delete notes </li>
+                                <li>App data is stored in localStorage</li>
+                            </ul>
+                            <div class="projectLinks">
+                                <a href="https://faraimajor.github.io/note-app/" class="liveLink" target="_blank" rel="noopener noreferrer">Live</a>
+                                <a href="https://github.com/FaraiMajor/note-app" class="repoLink" target="_blank" rel="noopener noreferrer">Repo</a>
+                            </div>
+                        </div>
+                    </article>
+                    <article class="workBox">
+                        <div class="imageContainer">
                             <img src={tenzie} alt="tenzie" />
                         </div>
                         <div class="describeContainer">
                             <h3>Tenzies Game</h3>
-                            <h4>Technologies Used: <span class="techStack">React</span>, <span class="techStack">API</span>, <span class="techStack">CSS</span></h4>
+                            <h4>Technologies Used: <span class="techStack">React</span>, <span class="techStack">CSS</span></h4>
                             <ul>
                                 <li>Dynamic game interface utilizing 10 interchangable colored dice</li>
                                 <li>Integrated gaming mechanics enabling users to ‘roll the dice’ in consecutively timed rounds.</li>
@@ -48,24 +85,8 @@ export default function Projects() {
                             </div>
                         </div>
                     </article>
-                    <article class="workBox">
-                        <div class="imageContainer">
-                            <img src={weather} alt="weather" />
-                        </div>
-                        <div class="describeContainer">
-                            <h3>Weather Rangers</h3>
-                            <h4>Technologies Used: <span class="techStack">React</span>, <span class="techStack">REST API</span>, <span class="techStack">NodeJS</span>, <span class="techStack">PostgresSQL</span></h4>
-                            <ul>
-                                <li>Completely built with PERN stack</li>
-                                <li>Displays daily weather forecast for your current location using geotags </li>
-                                <li>Allows users to create an account and save their favorite locations in their account</li>
-                            </ul>
-                            <div class="projectLinks">
-                                <a href="https://weather-rangers-frontend.herokuapp.com/" class="liveLink" target="_blank" rel="noopener noreferrer">Live</a>
-                                <a href="https://github.com/FaraiMajor/WeatherRangers_frontend" class="repoLink" target="_blank" rel="noopener noreferrer">Repo</a>
-                            </div>
-                        </div>
-                    </article>
+
+
                 </div>
             </section>
         </div>
