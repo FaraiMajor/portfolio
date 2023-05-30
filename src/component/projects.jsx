8fcb6e526bc7c0ad7de1022e4978e738b0c2ab42
Farai Major <faraimutukumira@gmail.com>
2023-05-30 16:37:19 -0400

    added a new project

--- src/component/projects.jsx
+++ src/component/projects.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import weather from '../assets/weatherranger.png';
 import memories from '../assets/memories.png';
-import tenzie from '../assets/tenzies.png'
 import noteapp from '../assets/note-app.png'
+import veryrare from '../assets/veryrare.png'
 import "../styles/projects.css"
 
 export default function Projects() {
@@ -13,6 +13,25 @@ export default function Projects() {
                     <h2><span>MY</span> PROJECTS</h2>
                 </div>
                 <div className="projectsContainer sectionContainer">
+                    <article className="workBox">
+                        <div className="imageContainer">
+                            <img src={veryrare} alt="tenzie" />
+                        </div>
+                        <div className="describeContainer">
+                            <h3>VeryRare E-Commerce</h3>
+                            <h4>Technologies Used: <span className="techStack">React ,</span><span className="techStack">Styled Components, Firebase, Stripe</span></h4>
+                            <ul>
+                                <li>The website allows users to browse and purchase products securely.</li>
+                                <li>Firebase's authentication service enabled users to sign up, log in, and securely access their account information.</li>
+                                <li>Firebase's real-time database was utilized to store and manage product data, customer details, and order information.</li>
+                                <li>To handle payments,  the app integrated the Stripe payment gateway, which provides a seamless and secure checkout experience for customers.</li>
+                            </ul>
+                            <div className="projectLinks">
+                                <a href="https://very-rare.netlify.app/" className="liveLink" target="_blank" rel="noopener noreferrer">Live Site</a>
+                                <a href="https://github.com/FaraiMajor/reactjs-ecommerce" className="repoLink" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
+                            </div>
+                        </div>
+                    </article>
                     <article className="workBox">
                         <div className="imageContainer">
                             <img src={weather} alt="weather" />
@@ -26,8 +45,8 @@ export default function Projects() {
                                 <li>Allows users to create an account and save their favorite locations in their account</li>
                             </ul>
                             <div className="projectLinks">
-                                <a href="https://weather-rangers-frontend.herokuapp.com/" className="liveLink" target="_blank" rel="noopener noreferrer">Live</a>
-                                <a href="https://github.com/FaraiMajor/WeatherRangers_frontend" className="repoLink" target="_blank" rel="noopener noreferrer">Repo</a>
+                                <a href="https://weather-rangers-frontend.herokuapp.com/" className="liveLink" target="_blank" rel="noopener noreferrer">Live Site</a>
+                                <a href="https://github.com/FaraiMajor/WeatherRangers_frontend" className="repoLink" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
                             </div>
                         </div>
                     </article>
@@ -44,8 +63,8 @@ export default function Projects() {
                                 <li>App data is stored in localStorage</li>
                             </ul>
                             <div className="projectLinks">
-                                <a href="https://faraimajor.github.io/note-app/" className="liveLink" target="_blank" rel="noopener noreferrer">Live</a>
-                                <a href="https://github.com/FaraiMajor/note-app" className="repoLink" target="_blank" rel="noopener noreferrer">Repo</a>
+                                <a href="https://faraimajor.github.io/note-app/" className="liveLink" target="_blank" rel="noopener noreferrer">Live Site</a>
+                                <a href="https://github.com/FaraiMajor/note-app" className="repoLink" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
                             </div>
                         </div>
                     </article>
@@ -63,27 +82,8 @@ export default function Projects() {
                                 <li>App stores data usiong mongoDB</li>
                             </ul>
                             <div className="projectLinks">
-                                <a href="https://github.com/FaraiMajor/memoirs" className="liveLink" target="_blank" rel="noopener noreferrer">Live</a>
-                                <a href="https://github.com/FaraiMajor/memoirs" className="repoLink" target="_blank" rel="noopener noreferrer">Repo</a>
-                            </div>
-                        </div>
-                    </article>
-
-                    <article className="workBox">
-                        <div className="imageContainer">
-                            <img src={tenzie} alt="tenzie" />
-                        </div>
-                        <div className="describeContainer">
-                            <h3>Tenzies Game</h3>
-                            <h4>Technologies Used: <span className="techStack">React ,</span><span className="techStack">CSS</span></h4>
-                            <ul>
-                                <li>Dynamic game interface utilizing 10 interchangable colored dice</li>
-                                <li>Integrated gaming mechanics enabling users to ‘roll the dice’ in consecutively timed rounds.</li>
-                                <li>Game store data on best time, number of dice rolls in localStorage</li>
-                            </ul>
-                            <div className="projectLinks">
-                                <a href="https://faraimajor.github.io/tenzies_game/" className="liveLink" target="_blank" rel="noopener noreferrer">Live</a>
-                                <a href="https://github.com/FaraiMajor/tenzies_game" className="repoLink" target="_blank" rel="noopener noreferrer">Repo</a>
+                                <a href="https://github.com/FaraiMajor/memoirs" className="liveLink" target="_blank" rel="noopener noreferrer">Live Site</a>
+                                <a href="https://github.com/FaraiMajor/memoirs" className="repoLink" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
                             </div>
                         </div>
                     </article>
